c0c5711ea9a35252105679beb51a4ea36bd49627
Martin Prokes <martin.prokes@profiq.cz>
2017-04-05 22:00:57 +0200

    Serialize commands in promises

--- bin/cmdTest.js
+++ bin/cmdTest.js
@@ -13,55 +13,48 @@ configParser.then((configs,err)=> {
         console.error(err);
     }
     var promises = configs.map(config => {
-        return new Promise(function(resolve, reject) {
-            var cmds = config.split(' ');
-            var cmd = cmds[0];
-            cmds.shift();
-            const cmdSpawn = spawn(cmd, cmds);
-            const logData =  (data)=>{
-                console.log(data.toString());
-                if(data.indexOf('[ERR]')>=0){
-                    let command = cmdSpawn.spawnargs.join(' ');
-                    console.log('Error when running cmd :', command, '\nError = ', data.toString());
-                    cmdSpawn.stdout.removeListener('data',logData);
-                    return resolve(command);
-                }
-            };
-            cmdSpawn.stdout.on('data', logData /*(data)=>{
-                //console.log('cmd = '+cmds+' data : '+ data);
-                /!*console.log(data.toString());
-                if(data.indexOf('[ERR]')>=0){
-                    let command = cmdSpawn.spawnargs.join(' ');
-                    console.log('Error when running cmd :', command, '\nError = ', data.toString());
-                    cmdSpawn.stdout.removeListener('data');
-                    return resolve(command);
-                }*!/
-            }*/);
-            cmdSpawn.stderr.on('data', data => {
-                console.log('error');
-                return reject(data);
+        return () => {
+            return new Promise(function (resolve, reject) {
+                var cmds = config.split(' ');
+                var cmd = cmds.shift();
+                const cmdSpawn = spawn(cmd, cmds);
+                let command = cmdSpawn.spawnargs.join(' ');
+                console.log('running command : ', command);
+                const logData = (data)=> {
+                    if (data.indexOf('[ERR]') >= 0) {
+                        console.log('Error when running cmd :', command, '\nError = ', data.toString());
+                        //cmdSpawn.stdout.removeListener('data', logData);
+                        cmdSpawn.kill();
+                        return resolve();
+                    }
+                };
+                cmdSpawn.stdout.on('data', logData);
+                cmdSpawn.stderr.on('data', data => {
+                    console.log('error');
+                    return reject(data);
+                });
+                cmdSpawn.on('close', code => {
+                    return resolve(code);
+                })
             });
-            cmdSpawn.on('close', code => {
-                resolve(code);
-            })
-        });
+        }
 
     });
-    Promise.all(promises)
-        .then(function(exitCodes) { console.log('all finished'); console.log(exitCodes); })
-        .catch(console.error);
-});
 
-/*
-const sencha = spawn('sencha',['-cwd',cwDir,'-sdk',fwDir+'\\ext-6.2.2.113','generate','app','myApp','myApp']);
-sencha.stdout.on('data', (data)=>{
-    if(data.indexOf('[ERR]')>=0){
-        console.log('following command failed : ' , sencha.spawnargs.join(' '));
-        console.log(`with error  : ${data}`);
-        sencha.kill();
+    // serialize our fns
+    function serialize(fns) {
+        var result = Promise.resolve();
+        fns.forEach((f)=>{
+            result = result.then(f);
+        });
+        return result;
     }
 
+
+    var res = serialize(promises);
+
+    res.then(function (done,err) {
+        console.log('all tests done');
+    }, console.error);
+
 });
-sencha.stdout.on('close', (code)=>{
-    console.log('exit code : ', code);
-});*/
